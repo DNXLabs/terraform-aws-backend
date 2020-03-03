c6ce61d2b8599f052672111bd53320d548b9d378
Arthur Diniz <arthurbdiniz@gmail.com>
2020-03-03 17:27:51 +1100

    Updates to terraform 0.12 syntax
    Closes: #1
    
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- backend_roles.tf
+++ backend_roles.tf
@@ -2,30 +2,36 @@
 resource "aws_iam_role" "backend-all" {
   name               = "terraform-backend"
   description        = "Allows access to all Terraform workspaces"
-  assume_role_policy = "${data.aws_iam_policy_document.backend-assume-role-all.json}"
+  assume_role_policy = data.aws_iam_policy_document.backend-assume-role-all.json
 }
 
 resource "aws_iam_role_policy" "backend-all" {
   name   = "terraform-backend"
-  policy = "${data.aws_iam_policy_document.iam-role-policy.json}"
+  policy = data.aws_iam_policy_document.iam-role-policy.json
   role   = "terraform-backend"
 
-  depends_on = ["aws_iam_role.backend-all"]
+  depends_on = [aws_iam_role.backend-all]
 }
 
 #These roles are limited to their specific workspace through the use of S3 resource permissions
 resource "aws_iam_role" "backend-restricted" {
-  count              = "${length(var.workspaces)}"
-  name               = "terraform-backend-${element(var.workspaces, count.index)}"
-  description        = "Allows access to the ${element(var.workspaces, count.index)} Terraform worksapce"
-  assume_role_policy = "${element(data.aws_iam_policy_document.backend-assume-role-restricted.*.json, count.index)}"
+  count       = length(var.workspaces)
+  name        = "terraform-backend-${element(var.workspaces, count.index)}"
+  description = "Allows access to the ${element(var.workspaces, count.index)} Terraform worksapce"
+  assume_role_policy = element(
+    data.aws_iam_policy_document.backend-assume-role-restricted.*.json,
+    count.index,
+  )
 }
 
 resource "aws_iam_role_policy" "backend-restricted" {
-  count  = "${length(var.workspaces)}"
-  name   = "terraform-backend-${element(var.workspaces, count.index)}"
-  policy = "${element(data.aws_iam_policy_document.iam-role-policy-restricted.*.json, count.index)}"
-  role   = "terraform-backend-${element(var.workspaces, count.index)}"
+  count = length(var.workspaces)
+  name  = "terraform-backend-${element(var.workspaces, count.index)}"
+  policy = element(
+    data.aws_iam_policy_document.iam-role-policy-restricted.*.json,
+    count.index,
+  )
+  role = "terraform-backend-${element(var.workspaces, count.index)}"
 
-  depends_on = ["aws_iam_role.backend-restricted"]
+  depends_on = [aws_iam_role.backend-restricted]
 }
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "state_bucket_arn" {
-  value = "${aws_s3_bucket.backend.arn}"
+  value = aws_s3_bucket.backend.arn
 }
 
 output "dynamo_lock_table" {
-  value = "${aws_dynamodb_table.lock.id}"
+  value = aws_dynamodb_table.lock.id
 }
 
 output "iam_roles" {
-  value = "${concat(aws_iam_role.backend-all.*.arn, aws_iam_role.backend-restricted.*.arn)}"
+  value = concat(
+    aws_iam_role.backend-all.*.arn,
+    aws_iam_role.backend-restricted.*.arn,
+  )
 }
