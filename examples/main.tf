5b9ebc82d79b38c0da078eb859f1e2727b5d5901
Mike Brown <mdbntl@gmail.com>
2021-03-17 14:02:24 -0500

    Add provider stanza to examples main.tf (#6)
    * add provider stanza to examples main.tf
    * Remove the offending bucket_region reference

--- examples/main.tf
+++ examples/main.tf
@@ -1,8 +1,16 @@
+provider "aws" {
+  # the following assumes you've defined a separate profile
+  # named [project] in ~/.aws/credentials  (apart from the 
+  # usual [default])
+  # 
+  profile = "project"
+  region = "ap-southeast-2"
+}
+
 module "tf-backend" {
   source = "../"
 
   bucket_prefix = "example1"
-  bucket_region = "ap-southeast-2"
 
   bucket_sse_algorithm = "AES256"
 
